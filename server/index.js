597157187e7668b6bc99f7d778813ee4f27fd085
Ryan Zigler <rpzigler@gmail.com>
2020-08-22 19:30:03 -0500

    deployment commit

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const favicon = require('serve-favicon');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const path = require('path');
 const app = express();
 const PORT = 3333;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.use(favicon(path.join(__dirname, './public', 'favicon.ico')));
 app.use('/hostels/:hostel_id', express.static(path.join(__dirname, '../public')));
 
 //app.use('/api', createProxyMiddleware({ target: 'http://www.example.org', changeOrigin: true }));
@@ -12,13 +19,13 @@ app.use('/hostels/:hostel_id', express.static(path.join(__dirname, '../public'))
 app.use('/api/hostels/:hostel_id/images', createProxyMiddleware({target: 'http://localhost:3007', changeOrigin: true}));
 
 /* ======= property info service ======= */
-app.use('/house/:id/hostel', createProxyMiddleware({target: 'http://localhost:3000', changeOrigin: true}));
+app.use('api/house/:id/hostel', createProxyMiddleware({target: 'http://localhost:3000', changeOrigin: true}));
 
-app.use('/house/:id/description', createProxyMiddleware({target: 'http://localhost:3000', changeOrigin: true}));
+app.use('api/house/:id/description', createProxyMiddleware({target: 'http://localhost:3000', changeOrigin: true}));
 
-app.use('/house/:id/rules', createProxyMiddleware({target: 'http://localhost:3000', changeOrigin: true}));
+app.use('api/house/:id/rules', createProxyMiddleware({target: 'http://localhost:3000', changeOrigin: true}));
 
-app.use('/house/:id/address', createProxyMiddleware({target: 'http://localhost:3000', changeOrigin: true}));
+app.use('api/house/:id/address', createProxyMiddleware({target: 'http://localhost:3000', changeOrigin: true}));
 
 /* ======= availability service ======= */
 app.use('/api/hostel/:hostelId/rooms', createProxyMiddleware({target: 'http://localhost:3009', changeOrigin: true}));
